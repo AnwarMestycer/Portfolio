bdc390935ff27d00cc90f0c8f5aeb495e4b9e22c
AnwarMestycer <anwarmestycer0@.com>
2024-07-28 17:14:50 +0100

    about ready

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -256,17 +256,17 @@ const Resume = () => {
               value="about"
               className="w-full text-center xl:text-left"
             >
-              <div>
+              <div className="flex flex-col gap-[30px] xl:ml-10"> 
                 <h3 className="text-4xl font-bold">{about.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                     {about.description}
                   </p>
-                  <ul>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[680px] mx-auto xl:mx-0">
                     {about.info.map((item, index)=> {
                       return (
-                        <li key={index}>
-                          <span>{item.fieldName}</span>
-                          <span>{item.fieldValue}</span>
+                        <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                          <span className="text-white/60">{item.fieldName}</span>
+                          <span className="text-xl0">{item.fieldValue}</span>
                         </li>
                       )
                     })}
